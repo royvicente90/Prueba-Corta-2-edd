c9d100fa209faff564a99d741c59e59c9112f5cb
royvicente90 <r.roy@correo.unimete.edu.ve>
2020-11-27 19:14:06 -0400

    Primer Commit - Testing -

---
+++ Prueba-Corta-2/src/PruebaCorta2/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PruebaCorta2;
+
+/**
+ *
+ * @author royvi
+ */
+public class Main {
+    public static void main(String [] args) {
+        
+        System.out.println("Testing");
+        
+    }
+    
+}
