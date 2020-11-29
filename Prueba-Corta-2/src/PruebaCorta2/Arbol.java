784410ddc9cc68c13d034bda8aeb4d189aa9df3c
Andres Espinosa <andres@gogamelab.com>
2020-11-29 18:55:36 -0500

    nuevos metodos a los nodos

--- Prueba-Corta-2/src/PruebaCorta2/Arbol.java
+++ Prueba-Corta-2/src/PruebaCorta2/Arbol.java
@@ -10,5 +10,24 @@ package PruebaCorta2;
  * @author andresespinoza
  */
 public class Arbol {
+    Nodo raiz = new Nodo();
     
+    /*
+     * Metodos para insertar, eliminar, buscar y reorganizar (balancear) el arbol AVL
+     */
+    
+    public boolean balanceado(){
+        return false;
+    }
+    
+    public void agregarHijo(Nodo nuevoNodo){
+        //Caso en el que el arbol es vacio
+        if(this.raiz == null){
+            this.raiz = nuevoNodo;
+        }
+        else{
+            //Se va navegando para comenzar a 
+            if(raiz.getValor() > nuevoNodo.getValor())
+        }
+    }
 }
